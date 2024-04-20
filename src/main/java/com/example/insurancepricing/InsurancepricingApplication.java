82b67659738014b2efde4e0d5f25a22dff5101d0
José Vasconcelos <jgv.junior@gmail.com>
2024-04-20 01:01:27 -0300

    feat: set up spring boot framework

---
+++ src/main/java/com/example/insurancepricing/InsurancepricingApplication.java
@@ -0,0 +1,13 @@
+package com.example.insurancepricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancepricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurancepricingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/insurancepricing/InsurancepricingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insurancepricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurancepricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
